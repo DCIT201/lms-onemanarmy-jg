562f0dd3500b60d7a7679ed7c7f7cdee07dbcb39
Gilead Kwadwo Aboagye <gileadaboagye@gmail.com>
2024-12-19 11:18:19 +0000

    library management

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -10,5 +10,26 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        //Creating books
+       Book book1 = new Book("1984","George Orwell");
+       Book book2 = new Book("To kill a Mockingbird","Harper Lee");
+
+       //Adding books to library
+  library.addBook(book1);
+  library.addBook(book2);
+
+  //Creating patrons.
+  Patron patron1= new Patron("Alice",1);
+  Patron patron2 = new Patron("Bob",2);
+
+   //Adding patrons to the library
+        library.addPatron(patron1);
+        library.addPatron(patron2);
+
+   //Simulating operations.
+        library.checkOutBook("1984",patron1);
+        library.checkOutBook("1984",patron2);  //should fail
+        library.returnBook("1984");
+        library.checkOutBook("1984",patron2); //should succeed
     }
 }
--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,5 +1,29 @@
-public class Book {
+public class Book{
 
     // This is your working class.
+    private String title;
+    private String author;
+   
+    private boolean isAvailable;
+
+    public Book(String title, String author) {
+       this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+  public void setAvailable(boolean available) {
+        isAvailable = available;
     
 }
